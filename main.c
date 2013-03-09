ddfa45c013c0338fcb51753c00acc601a512b005
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2013-03-09 12:22:51 +0100

    Implementation of printfVector

--- main.c
+++ main.c
@@ -7,6 +7,13 @@
 #include <stdio.h>
 #include "mpi.h"
 
+void printfVector(double vec[], int length){
+  for(int i=0; i<length; i++) {
+    printf("%1.1f\t",vec[i]);
+  }
+  printf("\n");
+}
+
 int main(int argc, char* argv[]) {
 
   int rank;
@@ -15,6 +22,20 @@ int main(int argc, char* argv[]) {
   MPI_Init(&argc, & argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+  int N = 10;
+  double vector[N];
+  if(rank==0){
+    for(int i=0; i<N; i++){
+      vector[i]=i;
+    }
+  }
+
+  if(rank==0){
+    printfVector(vector,N);
+  }
+
+
 
   MPI_Finalize();
 

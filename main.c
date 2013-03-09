4523cf29599e1a04ed90ce475934756eb4fa23cd
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2013-03-09 12:36:26 +0100

    Implementation printfMatrix

--- main.c
+++ main.c
@@ -7,11 +7,19 @@
 #include <stdio.h>
 #include "mpi.h"
 
+int N=10;
+
 void printfVector(double vec[], int length){
   for(int i=0; i<length; i++) {
     printf("%1.1f\t",vec[i]);
   }
   printf("\n");
+}
+
+void printfMatrix(double matrix[][N], int nrows) {
+  for(int i=0; i<nrows; i++) {
+    printfVector(matrix[i],N);
+  }
 }
 
 int main(int argc, char* argv[]) {
@@ -23,19 +31,17 @@ int main(int argc, char* argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-  int N = 10;
-  double vector[N];
+  double matrix[2][N];
   if(rank==0){
-    for(int i=0; i<N; i++){
-      vector[i]=i;
+    for(int j=0; j<N; j++){
+      matrix[0][j] = j;
+      matrix[1][j] = 2*j;
     }
   }
 
   if(rank==0){
-    printfVector(vector,N);
+    printfMatrix(matrix,2);
   }
-
-
 
   MPI_Finalize();
 
